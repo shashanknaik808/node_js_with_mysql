13c9d9008920a76f4e7e849923d2ee760df9dea4
Shashank Naik <shashanknaik808@gmail.com>
2023-10-06 11:42:25 +0530

    Database Created

--- index.js
+++ index.js
@@ -12,20 +12,28 @@ const db = mysql.createConnection({
 
 db.connect(err => {
     if (err) console.log("There is an error while connecting" + err);
-    
+
     app.listen(4000);
-    
+
     console.log
-    ("DB Connected" + db.threadId);
-    
+        ("DB Connected" + db.threadId);
+
     console.log
-    ("Server started at port 4000");
-    
+        ("Server started at port 4000");
+
 });
 
 
 const app = express();
 
 app.get('/createdb', (req, res) => {
-    
+
+    let sql = 'CREATE DATABASE nodemysql';
+
+    db.query(sql, error => {
+        if (error) {
+            return error
+        }
+        res.send('Database created')
+    });
 });
